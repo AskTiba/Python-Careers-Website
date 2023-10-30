dbe9cbbc7789fc6bc7a54f3763647f1565891a25
AskTiba <AskTiba.anthonyngisiro@gmail.com>
2023-10-30 13:32:34 +0300

    Basic css and rendering re-usable components

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jobs = [
+    {   
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Singapore"
+    },
+    {   
+        "id": 2,
+        "title": "Project Manager",
+        "location": "Ethiopia"
+    },
+    {   
+        "id": 3,
+        "title": "Dentist",
+        "location": "Sweden"
+    },
+    {   
+        "id": 4,
+        "title": "Accountant",
+        "location": "Libya"
+    },
+]
 
 @app.route('/')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', work=jobs, company_name="Jolly Job Seekers, Inc.")
 
 if __name__ == '__main__':
     app.run(debug=True)
